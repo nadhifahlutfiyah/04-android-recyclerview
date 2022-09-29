280f8cbf23f76a0ac5f7cddde6d449d7ac1777cd
= <=>
2022-09-29 15:36:23 +0700

    final

--- app/src/main/java/jti/nadhifah/recyclerview/WordListAdapter.java
+++ app/src/main/java/jti/nadhifah/recyclerview/WordListAdapter.java
@@ -11,23 +11,28 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.LinkedList;
 
-public class WordListAdapter extends
-        RecyclerView.Adapter<WordListAdapter.WordViewHolder>  {
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder>  {
     private final LinkedList<String> mWordList;
     private LayoutInflater mInflater;
 
+    public WordListAdapter(Context context,
+                           LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
     @NonNull
     @Override
     public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View mItemView = mInflater.inflate(R.layout.wordlist_item,
-                    parent, false);
-            return new WordViewHolder(mItemView, this);
+        View mItemView = mInflater.inflate(R.layout.wordlist_item,
+                parent, false);
+        return new WordViewHolder(mItemView, this);
     }
+
 
     @Override
     public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
-            String mCurrent = mWordList.get(position);
-            holder.wordItemView.setText(mCurrent);
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
     }
 
     @Override
@@ -35,20 +40,24 @@ public class WordListAdapter extends
         return mWordList.size();
     }
 
-    class WordViewHolder extends RecyclerView.ViewHolder {
+    class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public final TextView wordItemView;
         final WordListAdapter mAdapter;
         public WordViewHolder(View itemView, WordListAdapter adapter) {
             super(itemView);
             wordItemView = itemView.findViewById(R.id.word);
             this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            // Get the position of the item that was clicked.
+            int mPosition = getLayoutPosition();
+            String element = mWordList.get(mPosition);
+            mWordList.set(mPosition, "Clicked! " + element);
+            mAdapter.notifyDataSetChanged();
         }
     }
-
-
-    public WordListAdapter(Context context,
-                           LinkedList<String> wordList) {
-        mInflater = LayoutInflater.from(context);
-        this.mWordList = wordList;
-    }
 }
+
